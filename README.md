11d21cfd6c4dafd29e39b256dfe61de88018b102
Amninder Dhillon <amninder.dhillon112@gmail.com>
2017-12-09 02:25:47 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Predict is a song will be popular or not using machine learning
+# Predict Song Popularity
+##Introduction
+In this project, I will be using machine learning to classify a song as popular or not popular. The dataset used in this project is called [million song dataset](https://labrosa.ee.columbia.edu/millionsong/). As the name suggests, dataset contains one million contemporary music tracks from 1920s to 2010. 
+
---
+++ src/msd.py
@@ -0,0 +1,51 @@
+import os
+import glob
+import time
+import json
+import numpy as np
+import pandas as pd
+import hdf5_getters 
+
+def get_available_methods():
+	methods = [i for i in dir(hdf5_getters)]
+	return [i for i in methods if (("get_" in i) and ("get_num_songs"!=i))]
+
+def read_metadata(h5):
+	hdf5_file = hdf5_getters.open_h5_file_read(h5)
+	methods = get_available_methods()
+	metadata = {}
+	for method in methods:
+		result = hdf5_getters.__getattribute__(method)(hdf5_file)
+		if isinstance(result,np.ndarray):
+			result = json.dumps(result.tolist())
+		metadata[method[4:]] = result 
+
+	hdf5_file.close()
+	return metadata
+
+# def create_csv(metadata):
+
+def create_summary_csv(rootdir,csv_file):
+	first_run = True
+	ext = "h5"
+	df = None
+	df_temp = None;
+	for root,dirs,files in os.walk(rootdir):
+		files = glob.glob(os.path.join(root,"*"+ext))
+		for f in files:
+			metadata = read_metadata(f)
+			if first_run:
+				df = pd.DataFrame.from_dict(metadata,orient='index').transpose()
+				first_run = False
+			else:
+				df_temp = pd.DataFrame.from_dict(metadata,orient='index').transpose()
+				df = df.append(df_temp)
+	df.reset_index(drop=True,inplace=True)
+	df.to_csv(csv_file)
+	return df
+
+file = "C:\Users\Amninder\Downloads\MillionSongSubset\data\A\A\A\TRAAAAW128F429D538.h5"
+csv_file = "msd_subset.csv"
+start_time = time.time()
+create_summary_csv("C:\Users\Amninder\Downloads\MillionSongSubset\data\A",csv_file)
+print "Finished creating csv in %f seconds"%(time.time()-start_time)
